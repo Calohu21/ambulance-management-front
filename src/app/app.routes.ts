10e2d08d0a47a5d5f4467e15017b42fae4561688
Carmelo Lozano Hurtado <carmelopola@hotmail.com>
2025-11-11 22:56:52 +0100

    Revert "Feat/auth token managment"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './features/auth/pages/login-page/login-page';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: LoginPage,
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./features/auth/auth.routes'),
-  },
-];
+export const routes: Routes = [];
--- src/environment/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.ambulance-management.com/api',
-  apiTimeout: 15000,
-  enableLogs: false,
-  jwtTokenKey: 'ambulance_app_token',
-  appName: 'Sistema de Gestión de Ambulancias',
-  version: '1.0.0',
-};
