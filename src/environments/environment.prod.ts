f4da49b1cded4ac98bb8fe140fc8d01bae910989
Carmelo <carmelopola@hotmail.com>
2025-11-11 15:07:29 +0100

    chore(environments): implement environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8080/api',
+  jwtTokenKey: 'ambulance_app_token',
+  appName: 'Sistema de Gestión de Ambulancias',
+  version: '1.0.0',
+};
