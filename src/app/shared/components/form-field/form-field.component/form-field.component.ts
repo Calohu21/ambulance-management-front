0b837c1caeb7422f1dab877ca2719f2ed01f9c56
Carmelo <carmelopola@hotmail.com>
2025-11-12 14:45:10 +0100

    feat(auth): implements form login-page.ts and form-field.component.ts

---
+++ src/app/shared/components/form-field/form-field.component/form-field.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  imports: [],
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.css',
+})
+export class FormFieldComponent {
+  label = input<string>();
+  required = input<boolean>();
+  error = input<string>();
+  hint = input<string>();
+}
