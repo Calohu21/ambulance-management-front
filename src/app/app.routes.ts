6f94f288b441afb78b73ef13a69f1788cd9d5b1f
Carmelo <carmelopola@hotmail.com>
2025-11-15 19:18:55 +0100

    feat(table): add data-table.html and migrate daisyUi to flowbite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardPage } from '@features/dashboard/dashboard-page/dashboard-page';
 import { LoginPage } from '@features/auth/pages/login-page/login-page';
 import { MainLayout } from '@core/layout/main-layout/main-layout/main-layout';
+import { DataTable } from '@shared/components/data-table/data-table';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardPage,
+      },
+      {
+        path: 'table',
+        component: DataTable,
       },
     ],
   },
